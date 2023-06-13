e165b24fd9625c6bc7a47b3b36d8c5c948003602
shahmir2000 <shahmir5125507@gmail.com>
2023-06-13 14:10:38 +0500

    new commit

---
+++ server/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+
+import userRoutes from "./routes/users.js";
+import authRoutes from "./routes/auths.js";
+import tweetRoutes from "./routes/tweets.js";
+
+const app = express();
+dotenv.config();
+
+const connect = () => {
+  mongoose.set("strictQuery", false);
+  mongoose
+    .connect(
+      "mongodb+srv://hearts_thief:shahmir143@cluster0.biv3c.mongodb.net/twitter-app?retryWrites=true&w=majority"
+    )
+    .then(() => {
+      console.log("connect to mongodb database");
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+
+app.use(cookieParser());
+app.use(express.json());
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/tweets", tweetRoutes);
+
+app.listen(8000, () => {
+  connect();
+  console.log("Listening to port 8000");
+});
